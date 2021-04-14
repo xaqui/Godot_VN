a7b0c3f696c1fae9b0d15369fc29977b23d136a9
xaqui <kamixaqui@gmail.com>
2021-04-14 22:24:15 +0200

    Switched to JSON scene parsing

--- dialogue/dialogue.gd
+++ dialogue/dialogue.gd
@@ -1,26 +1,31 @@
 extends Control
 
+var scene_file #Path to file containing dialogue data of current scene
 var data
 var assocs
+var NameLabel
 var LabelDialog
 var BackgroundImage
+var CharacterArea
+var ItemArea
 var DialogueSound
 var DialogueControl
 var BackgroundSound
 var ButtonImageNext
 var ButtonNext
 var ButtonPrev
-var Forks
 var Last
 var choicesHistory = [] # This saves player choices for dialogue control
 var nextActions
 var DirPath
+var page
 
-var text_margin = -32 # Looks better if its not at the total bottom
 
 var p
 
 func choice (n):
+	var dialogue = data.get("dialogue")
+	
 	choicesHistory.append (n)
 	for c in DialogueControl.get_children ():
 		DialogueControl.remove_child (c)
@@ -29,70 +34,95 @@ func choice (n):
 		DialogueControl.hide ()
 	ButtonNext.set_disabled (false)
 	ButtonImageNext.set_disabled (false)
-	get_node ("/root/dialogue_loader").page = Forks[n]
-	go_to_page (Forks[n])
+	for i in dialogue.size():
+		if dialogue[i].get("id") == n :
+			page = i
+			break
+	go_to_page (page)
 
 func go_to_page (i):
-	var x = data[i]
+	var x = data.dialogue[i]
 	var d = DynamicFontData.new ()
 	var f = DynamicFont.new ()
-	if x.get ("b"):
-		BackgroundImage.set_texture (load (x["b"]))
+	if x.get ("background"):
+		BackgroundImage.set_texture (load (x.get("background")))
 		clear_items () # We wont need extra items when changing scene
-	if x.get ("i"):
+	if x.get ("items"):
 		clear_items ()
-		for item in (x["i"]):
-			add_item (item[0], item[1], item[2])
-	if x.get ("t"):
-		LabelDialog.set_text (x["t"])
-	if x.get ("f"):
-		d.set_font_path ("res://" + x["f"])
+		for item in (x.get("items")):
+			add_item (item.get("path"), item.get("x"), item.get("y"))
+	if x.get ("items") == []:
+		clear_items ()
+	if x.get ("sprites"):
+		for sprite in (x.get("sprites")):
+			add_character (sprite.path, 120, 30)
+	if x.get ("sprites") == []:
+		clear_characters()
+	if x.get ("text"):
+		LabelDialog.set_bbcode (x.get("text"))
+	if x.get ("character"):
+		NameLabel.set_text (x.get("character"))
+	if x.get ("character") == "":
+		NameLabel.set_text ("")
+	if x.get ("font"):
+		d.set_font_path ("res://" + x.get("font"))
 		f.set_font_data (d)
 		LabelDialog.get_theme ().set_default_font (f)
-	if x.get ("c"):
-		LabelDialog.add_color_override ("font_color", Color (x["c"]))
-	if x.get ("ts"):
-		f = LabelDialog.get_theme ().get_default_font ()
-		f.set_size (x["ts"])
-		LabelDialog.get_theme ().set_default_font (f)
-		LabelDialog.set_margin (MARGIN_BOTTOM, text_margin)
-	if x.get ("d"):
+	if x.get ("sound"):
 		DialogueSound.stop ()
-		DialogueSound.set_stream (load (x["d"]))
+		DialogueSound.set_stream (load (x.get("sound")))
 		DialogueSound.get_stream ().set_loop (false)
 		DialogueSound.play ()
 	else:
 		DialogueSound.stop ()
-	if x.get ("s") and x["s"] != "":
+	if x.get ("sound") and x.get("sound") != "":
 		BackgroundSound.stop ()
-		BackgroundSound.set_stream (load (x["s"]))
+		BackgroundSound.set_stream (load (x.get("sound")))
 		BackgroundSound.play ()
-	elif x.get ("s") and x["s"] == "":
+	elif x.get ("sound") and x.get("sound") == "":
 		BackgroundSound.stop ()
-	if x.get ("g"):
-		Last = get_node ("/root/dialogue_loader").page
-		get_node ("/root/dialogue_loader").page = assocs[x["g"]] - 2 # Next will incrememnt
-	if x.get ("x"):
+	if x.get ("jump"):
+		Last = page
+		for i in range(data.dialogue.size()):
+			if data.dialogue[i].get("id") == x.get("jump") :
+				page = i - 1
+				break
+	if x.get ("choices"):
 		show_choice (x)
 	else:
-		Forks = null
 		for i in nextActions:
 			InputMap.action_add_event ("next", i)
 
 func add_item (img, posX, posY):
-	if (img == "EMPTY_SET") :
+	if (img == "") :
 		clear_items()
 		return
 	var r = load(img)
 	var t = TextureRect.new ()
 	t.set_texture (r)
-	BackgroundImage.add_child (t)
+	ItemArea.add_child (t)
+	t.set_margin (MARGIN_LEFT, posX) 
+	t.set_margin (MARGIN_TOP, posY) 
+	
+func add_character (img, posX, posY):
+	if (img == "") :
+		clear_characters()
+		return
+	var r = load(img)
+	var t = TextureRect.new ()
+	t.set_texture (r)
+	CharacterArea.add_child (t)
 	t.set_margin (MARGIN_LEFT, posX) 
 	t.set_margin (MARGIN_TOP, posY) 
 
 func clear_items ():
-	for c in BackgroundImage.get_children ():
-		BackgroundImage.remove_child (c)
+	for c in ItemArea.get_children ():
+		ItemArea.remove_child (c)
+		c.queue_free ()
+	
+func clear_characters ():
+	for c in CharacterArea.get_children ():
+		CharacterArea.remove_child (c)
 		c.queue_free ()
 
 func show_choice (x):
@@ -101,26 +131,20 @@ func show_choice (x):
 	ButtonNext.set_disabled (true)
 	for i in nextActions:
 		InputMap.action_erase_event ("next", i)
-	Forks = []
-	Last = get_node ("/root/dialogue_loader").page
-	var c = 0
-	for i in x["x"]:
-		var v = assocs[i[0]]
-		if v != null:
-			Forks.append (v - 1)
+	Last = page
+	for i in x.choices:
 			var b = Button.new ()
 			DialogueControl.add_child (b)
-			b.set_text (i[1])
+			b.set_text (i.get("text"))
 			var s = GDScript.new ()
-			s.set_source_code ("extends Button\nfunc _pressed():\n\tget_node(\"/root/Control\").choice(" + String (c) + ")")
+			s.set_source_code ("extends Button\n\nfunc _pressed():\n\tget_node(\"/root/Control\").choice('"+String(i.get("jump"))+"')")
 			s.reload ()
 			b.set_script (s)
-			c = c + 1
 
 func next ():
-	if get_node ("/root/dialogue_loader").page + 1 < data.size ():
-		get_node ("/root/dialogue_loader").page = get_node ("/root/dialogue_loader").page + 1
-		go_to_page (get_node ("/root/dialogue_loader").page)
+	if page + 1 < data.dialogue.size ():
+		page = page + 1
+		go_to_page (page)
 		ButtonPrev.set_disabled (false)
 	else:
 		var err = get_tree ().change_scene ("menu/menu.tscn")
@@ -128,9 +152,9 @@ func next ():
 			print("Can't change scene!")
 
 func prev ():
-	if get_node ("/root/dialogue_loader").page > 0:
-		get_node ("/root/dialogue_loader").page = get_node ("/root/dialogue_loader").page - 1
-		go_to_page (get_node ("/root/dialogue_loader").page)
+	if page > 0:
+		page = page - 1
+		go_to_page (page)
 	clear_items ()
 	ButtonNext.set_disabled (false)
 	if DialogueControl.get_children () != []:
@@ -139,85 +163,65 @@ func prev ():
 		for i in nextActions:
 			InputMap.action_add_event ("next", i)
 		DialogueControl.hide ()
-	var x = data[get_node ("/root/dialogue_loader").page]
-	if not x.get ("c"):
-		var i = get_node ("/root/dialogue_loader").page - 1
-		while not data[i].get ("c") and i >= 0:
-			i = i - 1
-		if (i >= 0):
-			LabelDialog.add_color_override ("font_color", Color (data[i]["c"]))
-	if not x.get ("b"):
-		var i = get_node ("/root/dialogue_loader").page - 1
-		while (not data[i].get ("b") and i >= 0):
+	var x = data.dialogue[page]
+	if not x.get ("background"):
+		var i = page - 1
+		while (not data.dialogue[i].get("background") and i >= 0):
 			i = i - 1
 		if i >= 0:
-			BackgroundImage.set_texture (load (data[i]["b"]))
-	if not x.get ("ts"):
-		var i = get_node ("/root/dialogue_loader").page - 1
-		while not data[i].get ("ts") and i >= 0:
+			BackgroundImage.set_texture (load (data.dialogue[i].get("background")))
+	if not x.get ("sound"):
+		var i = page - 1
+		while not data.dialogue[i].get ("sound") and i >= 0:
 			i = i - 1
-		if i >= 0:
-			var f = LabelDialog.get_theme ().get_default_font ()
-			f.set_size (data[i]["ts"])
-			LabelDialog.get_theme ().set_default_font (f)
-			LabelDialog.set_margin (MARGIN_BOTTOM, text_margin)
-	if not x.get ("s"):
-		var i = get_node ("/root/dialogue_loader").page - 1
-		while not data[i].get ("s") and i >= 0:
-			i = i - 1
-		if i >= 0 and load (data[i]["s"]).get_path () != BackgroundSound.get_stream ().get_path ():
+		if i >= 0 and load ((data.dialogue[i].get("sound")) != BackgroundSound.get_stream().get_path ()):
 			BackgroundSound.stop ()
-			if data[i]["s"] != "":
-				BackgroundSound.set_stream (load (data[i]["s"]))
+			if data.dialogue[i].get("sound") != "":
+				BackgroundSound.set_stream (load (data.dialogue[i].get("sound")))
 				BackgroundSound.play (0)
-	if not x.get ("i"):
-		var i = get_node ("/root/dialogue_loader").page - 1
-		while not data[i].get ("i") and i >= 0:
+	if not x.get ("items"):
+		var i = page - 1
+		while not data.dialogue[i].get ("items") and i >= 0:
 			i = i - 1
 		if i >= 0:
 			clear_items ()
-			for j in data[i]["i"]:
-				add_item (j[0], j[1], j[2])
+			for item in data.dialogue[i].items:
+				add_item (item.get("path"), item.get("x"), item.get("y"))
 
 func _ready ():
 	BackgroundImage = get_node ("Panel/PictNext/BackgroundImage")
-	LabelDialog = get_node ("Panel/LabelDialog")
+	NameLabel = get_node("Panel/TextBox/NameTag")
+	LabelDialog = get_node ("Panel/TextBox/RichTextLabel")
 	DialogueSound = get_node ("Panel/DialogueSound")
+	CharacterArea = get_node ("Panel/CharacterControl")
+	ItemArea = get_node ("Panel/ItemControl")
 	DialogueControl = get_node ("Panel/DialogueControl")
 	BackgroundSound = get_node ("Panel/BackgroundSound")
 	ButtonImageNext = get_node ("Panel/PictNext")
 	ButtonNext = get_node ("Panel/ButtonNext")
 	ButtonPrev = get_node ("Panel/ButtonPrev")
 	DirPath = OS.get_executable_path().get_base_dir()
+	scene_file = "dialogue/demo.json"
+	page = 0
 	var t = Theme.new ()
+	var tn = Theme.new ()
 	var d = DynamicFontData.new ()
 	d.set_font_path ("res://dialogue/fonts/font.ttf") # Need to override the bitmap font with a vector font
 	var f = DynamicFont.new ()
 	f.set_font_data (d)
-	f.set_size (12)
+	f.set_size (20)
 	t.set_default_font (f)
+	tn.set_default_font (f)
 	LabelDialog.set_theme (t)
-	LabelDialog.set_margin (MARGIN_BOTTOM, text_margin)
-	var parser = XMLParser.new ()
-	if parser.open ("res://dialogue/data.xml") != 0:
-		print ("Error Opening: ", "dialogue/data.xml")
-		return null
-	data = get_node ("/root/dialogue_loader").get_dialogue_data (parser)
+	NameLabel.set_theme(tn)
+	NameLabel.add_color_override("font_color","#001a33")
+	data = get_node ("/root/dialogue_loader").get_dialogue_data (scene_file)
 	if data == null:
-		print ("XML read failure")
-		get_tree ().quit ()
-	var meta = data[0]
-	if (meta.get ("name")):
-		get_node ("/root/dialogue_loader").game_name = meta["name"]
-	assocs = data[1]
-	data = data[2]
-	if data.size () == 0:
-		print ("No scene data found")
-		get_tree ().quit ()
+		print ("JSON read failure")
 	if(get_saves(DirPath) != []):
 		get_node ("Menu/CenterContainer/VBoxContainer/Load").set_disabled (false)
 	nextActions = InputMap.get_action_list ("next")
-	go_to_page (get_node ("/root/dialogue_loader").page) # Needed for loading saved games
+	go_to_page (page) # Needed for loading saved games
 	set_focus_mode (FOCUS_ALL)
 	grab_focus ()
 
@@ -304,11 +308,11 @@ func save_game(idx):
 		popup_err("Couldn't open save file in slot "+idx)
 		return false
 	else:
-		savedata.game = get_node ("/root/dialogue_loader").game_name
-		savedata.version = get_node ("/root/dialogue_loader").game_version
+		savedata.game = data.meta.get("game_name")
+		savedata.version = data.meta.get("version")
 		savedata.date = date
-		savedata.scene = get_node ("/root/dialogue_loader").datafile
-		savedata.page = get_node ("/root/dialogue_loader").page
+		savedata.scene = data.meta.get("scene")
+		savedata.page = page
 		if choicesHistory != []: 
 			for choice in choicesHistory:
 				savedata.choices_history[choice] = choicesHistory[choice]
@@ -333,13 +337,13 @@ func load_game (idx):
 		if (savedata.page < 0):
 			print("Load game failed - save file doesn't contain saved page!")
 			return false
-		if (savedata.game != get_node ("/root/dialogue_loader").game_name):
+		if (savedata.game != data.meta.game_name):
 			print("Error: incorrect save file for " + get_node ("/root/dialogue_loader").game_name)
 			return false
-		if (savedata.version != get_node ("/root/dialogue_loader").game_version):
+		if (savedata.version != data.meta.version):
 			print("Error: save file game version does not match current game version")
 			return false
-	get_node ("/root/dialogue_loader").page = savedata.page
+	page = savedata.page
 	prev () # Reload items that may not be specified on current page
 	next ()
 	return true
--- dialogue/dialogue_loader.gd
+++ dialogue/dialogue_loader.gd
@@ -1,209 +1,30 @@
 extends Node
 
-#tool
+var scene_data = {}
 
-var engine_version = "1.0"
-var game_name = ""
-var game_version = ""
-var game_url = ""
-var game_author = ""
-var datafile = "dialogue/data.xml"
-var page = 0
-var meta
-
-func _ready ():
-	pass
-
-func get_dialogue_meta_data (parser):
-	var m = {}
-	while parser.get_node_name () != "meta" and parser.get_node_name () != "scenes":
-		if parser.read () != OK:
-			return null
-	if parser.get_node_name () == "meta":
-		for i in parser.get_attribute_count ():
-			if (parser.get_attribute_name (i) == "name"):
-				game_name = parser.get_attribute_value (i)
-			if (parser.get_attribute_name (i) == "version"):
-				game_version = parser.get_attribute_value (i)
-			if (parser.get_attribute_name (i) == "url"):
-				game_url = parser.get_attribute_value (i)
-			if (parser.get_attribute_name (i) == "author"):
-				game_author = parser.get_attribute_value (i)
-			m[parser.get_attribute_name (i)] = parser.get_attribute_value (i)
-	return m
-	
-func get_dialogue_data (parser):
-	var e = 0
-	var data = []
-	var background
-	var text
-	var backgroundsound
-	var dialoguesound
-	var c = 0
-	var a = {}
-	var goto
-	var y
-	var id
-	var z
-	meta = get_dialogue_meta_data (parser)
-	var indentFilter = RegEx.new ()
-	indentFilter.compile ("[ \n\t]*")
-	if meta == null:
-		return null
-	while parser.get_node_name () != "scenes":
-		if parser.read () != OK:
-			return null
-	while e == 0:
-		e = parser.read ()
-		if parser.get_node_type () == XMLParser.NODE_ELEMENT and parser.get_node_name () == "scene_data":
-			var i = null
-			var d = {}
-			dialoguesound = null
-			backgroundsound = null
-			background = null
-			c = c + 1
-			goto = null
-			var x = []
-			if parser.has_attribute ("id"):
-				id = parser.get_named_attribute_value ("id")
-				a[id] = c
-				var l = parser.get_named_attribute_value ("id")
-				d["l"] = l
-			if parser.has_attribute ("goto"):
-				var g = parser.get_named_attribute_value ("goto")
-				d["g"] = g
-			if parser.has_attribute ("background"):
-				var b = parser.get_named_attribute_value ("background")
-				if b.is_rel_path ():
-					d["b"] = b
-				else:
-					print ("Invalid Background Image: ", b)
-			if parser.has_attribute ("text"): # Alternative way of doing it
-				d["t"] = parser.get_named_attribute_value ("text").xml_unescape ()
-			if parser.has_attribute ("font"):
-				var f = parser.get_named_attribute_value ("font")
-				if f.is_rel_path ():
-					d["f"] = f
-				else:
-					print ("Invalid Font Path: ", f)
-			if parser.has_attribute ("size"):
-				var ts = parser.get_named_attribute_value ("size")
-				if ts.is_valid_integer ():
-					d["ts"] = ts.to_int ()
-				else:
-					print ("Invalid Font Size: ", ts)
-			if parser.has_attribute ("color"):
-				var color = parser.get_named_attribute_value ("color")
-				if color.is_valid_html_color ():
-					d["c"] = color
-				else:
-					print ("Invalid Color: ", color)
-			if parser.has_attribute ("backgroundsound"):
-				var dx = parser.get_named_attribute_value ("backgroundsound")
-				if dx.is_rel_path ():
-					d["s"] = dx
-				else:
-					print ("Invalid Background Sound: ", dx)
-			if parser.has_attribute ("dialoguesound"):
-				var ds = parser.get_named_attribute_value ("dialoguesound")
-				if ds.is_rel_path ():
-					d["d"] = ds
-				else:
-					print ("Invalid Dialogue Sound: ", d)
-			e = parser.read ()
-			while parser.get_node_type () == XMLParser.NODE_TEXT and indentFilter.search (parser.get_node_data ()).get_string () == parser.get_node_data (): #
-				e = parser.read ()
-			if parser.get_node_type () == XMLParser.NODE_TEXT and not d.has ("t") and parser.get_node_data () != "": #
-				d["t"] = parser.get_node_data ().xml_unescape ()
-			e = parser.read ()
-			while parser.get_node_type () == XMLParser.NODE_ELEMENT and parser.get_node_name () == "choice":
-				if parser.has_attribute ("goto"):
-					y = parser.get_named_attribute_value ("goto")
-				e = parser.read ()
-				z = false
-				if parser.get_node_type () == XMLParser.NODE_TEXT:
-					z = parser.get_node_data ()
-					e = parser.read ()
-					e = parser.read ()
-					while parser.get_node_type () == XMLParser.NODE_TEXT and parser.get_node_data ().strip_edges ().length () == 0: #
-						e = parser.read ()
-				if z:
-					x.append ([y, z])
-			if x != []:
-				d["x"] = x				
-			if parser.get_node_type () == XMLParser.NODE_ELEMENT and parser.get_node_name () == "items":
-				i = []
-				e = parser.read ()
-				# FIXME - hack to "eat" extra text node when there's whitespace before first <item>
-				if parser.get_node_type () == XMLParser.NODE_TEXT:
-					e = parser.read ()
-				while parser.get_node_type () == XMLParser.NODE_TEXT and indentFilter.search (parser.get_node_data ()).get_string () == parser.get_node_data (): #
-					e = parser.read ()
-				while parser.get_node_type () == XMLParser.NODE_ELEMENT and parser.get_node_name () == "item":
-					var s = ""
-					if (parser.has_attribute ("image") and parser.get_named_attribute_value ("image").is_rel_path ()):
-						s = parser.get_named_attribute_value ("image")
-					y = 0
-					if (parser.has_attribute ("x") and parser.get_named_attribute_value ("x").is_valid_integer ()):
-						y = parser.get_named_attribute_value ("x").to_int ()
-					z = 0
-					if (parser.has_attribute ("y") and parser.get_named_attribute_value ("y").is_valid_integer ()):
-						z = parser.get_named_attribute_value ("y").to_int ()
-					e = parser.read ()
-					if parser.get_node_type () == XMLParser.NODE_TEXT:
-						e = parser.read ()
-					if s != "":
-						i.append ([s, y, z])
-					while parser.get_node_type () == XMLParser.NODE_TEXT and indentFilter.search (parser.get_node_data ()).get_string () == parser.get_node_data (): #
-						e = parser.read ()
-				d["i"] = i
-			data.append (d)
-	return [meta, a, data]
-
-func dump_data (datafile, data):
+func get_dialogue_data(file): #Pass path to file containing dialogue data of current scene
 	var f = File.new ()
-	if f.open (datafile, File.WRITE) != OK:
-		return false
-	f.store_string ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<meta")
-	for i in meta:
-		f.store_string ("\"".insert (0, (meta[i].insert (0, "=\"".insert (0, i.insert (0, " "))))))
-	f.store_line ("/>\n<scenes>")
-	for i in data:
-		f.store_string ("  <scene_data")
-		if i.has ("b"):
-			f.store_string ("\"".insert (0, i["b"].insert (0, " background=\"")))
-		if i.has ("f"):
-			f.store_string ("\"".insert (0, i["f"].insert (0, " font=\"")))
-		if i.has ("g"):
-			f.store_string ("\"".insert (0, i["g"].insert (0, " goto=\"")))
-		if i.has ("l"):
-			f.store_string ("\"".insert (0, i["l"].insert (0, " id=\"")))
-		if i.has ("ts"):
-			f.store_string ("\"".insert (0, String (i["ts"]).insert (0, " size=\"")))
-		if i.has ("c"):
-			f.store_string ("\"".insert (0, i["c"].insert (0, " color=\"")))
-		if i.has ("s"):
-			f.store_string ("\"".insert (0, i["s"].insert (0, " backgroundsound=\"")))
-		if i.has ("d"):
-			f.store_string ("\"".insert (0, i["d"].insert (0, " dialoguesound=\"")))
-		if i.has ("t") and (i.has ("i") and i["i"] != null) or (i.has ("x") and i["x"] != null):
-			f.store_string ("\"".insert (0, i["t"].xml_escape ().insert (0, " text=\"")))
-		f.store_string (">")
-		if i.has ("i") and i["i"] != null:
-			f.store_string ("\n    <items>\n")
-			for x in i["i"]:
-				f.store_string ("\"".insert (0, x[0].insert (0, "      <item image=\"")))
-				f.store_string ("\"".insert (0, String (x[1]).insert (0, " x=\"")))
-				f.store_line ("\"/>".insert (0, String (x[2]).insert (0, " y=\"")))
-			f.store_string ("    </items>\n  ")
-		if i.has ("x") and i["x"] != null:
-			for x in i["x"]:
-				f.store_string ("\"".insert (0, x[0].insert (0, "\n    <choice goto=\"")))
-				f.store_string ("</choice>".insert (0, String (x[1]).insert (0, ">")))
-			f.store_string ("\n  ")
-		if i.has ("t") and not (i.get ("i") or i.has ("x")):
-			f.store_string (i["t"].xml_escape ())
-		f.store_line ("</scene_data>")
-	f.store_line ("</scenes>")
-	f.close ()
-	return true
+	if f.open (file, File.READ) != OK:
+		return null 
+	scene_data = parse_json(f.get_as_text())
+	f.close()
+	
+	if (scene_data.get("meta") == null):
+		return null
+	else:
+		if(scene_data.meta.get("version") == null):
+			return null
+		elif(scene_data.meta.get("encoding") == null):
+			return null
+		elif(scene_data.meta.get("author") == null):
+			return null
+		elif(scene_data.meta.get("game_name") == null):
+			return null
+		elif(scene_data.meta.get("scene") == null):
+			return null
+	if (scene_data.get("dialogue") == null):
+		return null
+	elif (scene_data.get("dialogue") == []):
+		return null
+	
+	return scene_data
